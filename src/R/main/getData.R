2783582fa7335432c65ec5995ff97f95e5e0c1f8
Dan Negrey <dnegrey@gmail.com>
2020-12-30 11:37:26 -0500

    added getData calls using rtweet functions

--- src/R/main/getData.R
+++ src/R/main/getData.R
@@ -1,2 +1,88 @@
 getData <- function(dir) {
+    # load twitter api token
+    token <- readRDS("~/.twitter_token.rds")
+    # user screen name
+    user <- "blastpointinc"
+    # get timeline
+    RDataCreate(
+        dir = dir,
+        x = "timeline",
+        fun = get_timeline,
+        args = list(
+            user = user,
+            n = 2000,
+            token = token
+        )
+    )
+    # get followers
+    RDataCreate(
+        dir = dir,
+        x = "followers",
+        fun = lookup_users,
+        args = list(
+            users = get_followers(
+                user = user,
+                token = token
+            )$user_id,
+            token = token
+        )
+    )
+    # get friends
+    RDataCreate(
+        dir = dir,
+        x = "friends",
+        fun = lookup_users,
+        args = list(
+            users = get_friends(
+                users = user,
+                token = token
+            )$user_id,
+            token = token
+        )
+    )
+    # geo-code followers based on location
+    RDataCreate(
+        dir = dir,
+        x = "followers_geocode",
+        fun = function(followers) {
+            x <- followers[c("user_id", "location")]
+            x <- split(x, x$user_id)
+            y <- lapply(x, function(v){
+                ygc <- try(
+                    getGeoCode(v$location),
+                    silent = TRUE
+                )
+                lat <- as.numeric(NA)
+                lon <- lat
+                if (!inherits(ygc, "try-error")){
+                    lat <- ygc[["lat"]]
+                    lon <- ygc[["lon"]]
+                }
+                yd <- data.frame(
+                    user_id = v$user_id,
+                    lat = lat,
+                    lon = lon,
+                    stringsAsFactors = FALSE
+                )
+                return(yd)
+            })
+            y <- do.call(rbind, y)
+            row.names(y) <- NULL
+            return(y)
+        },
+        args = list(
+            followers = RDataUse(dir, "followers")
+        )
+    )
+    # get favorites
+    RDataCreate(
+        dir = dir,
+        x = "favorites",
+        fun = get_favorites,
+        args = list(
+            user = user,
+            n = 2000,
+            token = token
+        )
+    )
 }
