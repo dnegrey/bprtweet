169d9a9277521624f0f7c55d69b5aebf92f7c92a
Dan Negrey <dnegrey@gmail.com>
2021-01-04 15:22:58 -0500

    converted hashtags and mentions to linked lists

--- src/R/app/tweetsDT.R
+++ src/R/app/tweetsDT.R
@@ -21,6 +21,32 @@ tweetsDT <- function(x) {
     y <- x
     y$status_id <- sprintf(sl, y$status_id, y$status_id)
     y$created_at <- format(y$created, "%F %r")
+    y$hashtags <- unlist(lapply(y$hashtags, function(v, tmp = hl){
+        vo <- ""
+        if (!all(is.na(v))) {
+            v <- v[!is.na(v)]
+            va <- sprintf(tmp, "%2523", v, v)
+            va <- paste0("<li>", va, "</li>", collapse = "")
+            vo <- sprintf(
+                fmt = "<ul style='font-weight: bold;padding-left: 20px;'>%s</ul>",
+                va
+            )
+        }
+        return(vo)
+    }))
+    y$mentions_screen_name <- unlist(lapply(y$mentions_screen_name, function(v, tmp = al){
+        vo <- ""
+        if (!all(is.na(v))) {
+            v <- v[!is.na(v)]
+            va <- sprintf(tmp, v, v)
+            va <- paste0("<li>", va, "</li>", collapse = "")
+            vo <- sprintf(
+                fmt = "<ul style='font-weight: bold;padding-left: 20px;'>%s</ul>",
+                va
+            )
+        }
+        return(vo)
+    }))
     z <- datatable(
         data = y,
         class = "row-border hover nowrap cell-border",
